35efd098a8e185ecb55648aa7cfbbcdd71f2e755
Yazmin Gutierrez Hernandez <230447@utxicotepec.edu.mx>
2025-02-25 10:01:43 -0600

    Ejercicios Angular
    Se segmento la pagina principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatButtonModule} from '@angular/material/button';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { PageContentComponent } from "./components/page-content/page-content.component";
+import { BreadcrumpComponent } from "./components/breadcrump/breadcrump.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatButtonModule, NavbarComponent],
+  imports: [MatButtonModule, NavbarComponent, SidebarComponent, PageContentComponent, BreadcrumpComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
